55303bf3b9e5dd07e0ac64b1e4634a10517eb942
pasaye <pasayejoseph@yahoo.com>
2023-03-18 12:55:08 -0500

    Completed

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # MakeChange2
 
 ## Overview
-
+ - This project is cash register that returns small dominations from 20's to pennies.
+For instance, an item cost $10.67 and a customer gives me a 20 the program should 
+return exact change 9.33 in this form ( 1 five, 4 dollars, 1 quarter, 1 nickel, and 3 pennies). 
 
 ## Technologies Used
-
-
-
+ - Java
+ - Eclipse
+ - Git
 
 ## Lessons Learned
+ -Research is important: things like stack overflow, google, and w3school are incredible helpful.
+ It is likely that someone else has encounter the same bug/issues I had when writing my program. 
+ Researching those bugs/issues not only helped solve problems in my program, but allowed me to read several
+ explanations on how I created those bug/issues in my program. Reading all those explanations helped me learned
+ why my code didn't work. 
+ 
+ -Math is hard: I also learned that I am having a hard time conceptualizing math equations as code. I understand what needs
+ to be written, but I can't seem translate it into the code right away.
+ 
+ -Not every solution works: Not every solution to the same bug/issue is going to solve the ones I created in my program. For 
+ example, I was having a hard time getting my changedReturned variable to move it's remaining decimals down the code (33 -25 = 8).
+ I could get the code to issue the appropriate number of cents returned in the first line, but then the 1 nickel and 3 pennies would
+ disappear. Solutions told me to use something like (a = b / n) % .n %.n ) which didn't work for me. ultimately I had to moved around 
+ - , *, and Math.round() to fix my issues. However I wouldn't have known to play with operators if I hadn't previously researched other bugs.
+  
--- src/MakeChangeProject.java
+++ src/MakeChangeProject.java
@@ -1,9 +1,61 @@
 
+import java.util.Scanner;
+
 public class MakeChangeProject {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
+		
+		double itemPrice = 0.0;
+		double moneyRecieved = 0.0;
+		
+		System.out.println("What is the price of the item?");
+		itemPrice = kb.nextDouble();
+		System.out.println("How much money did the customer give you?");
+		moneyRecieved = kb.nextDouble();
+		
+		double changeReturned = moneyRecieved - itemPrice;
+	
+		if(itemPrice == moneyRecieved) {
+			System.out.println("No change need! Thank you have a nice day!");
+		} else if(moneyRecieved < itemPrice) {
+			System.err.println("Ask customer for the appropriate amount");
+		} else {
+		changeReturned = Math.round(changeReturned * 100.0) / 100.0;
+		 System.out.print("Please return: $" + changeReturned + " = ");
+		 exactChange(changeReturned);
+		}
+		kb.close();
 	}
-
+	
+	public static double exactChange(double changeReturned) { 
+		int twenty;
+		int ten;
+		int five;
+		int dollars;
+		int quarters;
+		int dimes;
+		int nickels;
+		int pennies;
+		
+		twenty = (int)(changeReturned / 20);
+		changeReturned -= twenty * 20;
+		ten = (int)(changeReturned / 10);
+		changeReturned -= ten * 10;
+		five = (int)(changeReturned / 5);
+		changeReturned -= five * 5;
+		dollars = (int) (changeReturned /1);
+		changeReturned -= dollars * 1;
+		quarters = (int)(changeReturned / 0.25); 
+		changeReturned -= quarters * 0.25;
+		dimes = (int)(changeReturned / .10);
+		changeReturned -= dimes * 0.10;
+		nickels = (int)(changeReturned / .05);
+		changeReturned -= nickels * .05;
+		pennies = (int)Math.round(changeReturned / .01);
+		
+		System.out.println(twenty + " Twenties " + ten + " Ten(s) " + five +" Five(s) " + dollars + " Dollar(s) " 
+				+ quarters + " Quarter(s) " + dimes + " Dime(s) " + nickels + " Nickel(s) " + pennies + " Pennies");
+			
+		return changeReturned;
+	} 
 }
